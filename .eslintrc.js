d7589a47f1aa200f2d42edd18c10e2a0a58e972e
Andrew Stanton <up683693@myport.ac.uk>
2020-05-20 19:13:53 +0100

    eslint config
    - work with vue
    - use tabs
    - enforce brackets on arrow functions

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	extends: [
+		'plugin:vue/recommended',
+	],
+	rules: {
+		'indent': ['error', 'tab'],
+		'arrow-parens': 2,
+		'vue/html-indent': [
+			'error',
+			'tab'
+		],
+	},
+};
